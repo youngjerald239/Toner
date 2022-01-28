eaacd27558d8dbaa0e7ecbd8bba0ebbc58943515
youngjerald239 <youngjerald239@gmail.com>
2022-01-27 20:17:28 -0600

    added heroicon buttons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -12,6 +12,19 @@ function Sidebar() {
             <div>
                 <button>
                     <HomeIcon className="h-5 w-5"/>
+                    <p>Home</p>
+                </button>
+                <button>
+                    <SearchIcon className="h-5 w-5"/>
+                    <p>Search</p>
+                </button>
+                <button>
+                    <LibraryIcon className="h-5 w-5"/>
+                    <p>Library</p>
+                </button>
+                <button>
+                    <PlusCircleIcon className="h-5 w-5"/>
+                    <p>More</p>
                 </button>
             </div>
         </div>
