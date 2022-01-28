16e9b31fdf250cba3bb44264e4c8aaea4f2e711a
youngjerald239 <youngjerald239@gmail.com>
2022-01-27 20:43:09 -0600

    styled buttons with tailwind

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,12 +3,15 @@ import {
     HomeIcon,
     SearchIcon,
     LibraryIcon,
-    PlusCircleIcon
+    PlusCircleIcon,
+    MusicNoteIcon,
+    HeartIcon,
+    RssIcon
 } from "@heroicons/react/solid"
 
 function Sidebar() {
     return (
-        <div className="text-gray-700">
+        <div className="text-gray-700 p-5 text-sm border-r border-gray-900">
             <div>
                 <button className="flex items-center space-x-2 
                 hover:text-white">
@@ -25,10 +28,28 @@ function Sidebar() {
                     <LibraryIcon className="h-5 w-5"/>
                     <p>My Library</p>
                 </button>
+                <hr className="border-t-[0.1px] border-gray-900" />
+
+                <button className="flex items-center space-x-2 
+                hover:text-white">
+                    <MusicNoteIcon className="h-5 w-5"/>
+                    <p>Create Playlist</p>
+                </button>
+                <button className="flex items-center space-x-2 
+                hover:text-white">
+                    <RssIcon className="h-5 w-5"/>
+                    <p>My Playlist</p>
+                </button>
+                <button className="flex items-center space-x-2 
+                hover:text-white">
+                    <HeartIcon className="h-5 w-5"/>
+                    <p>Favorite Songs</p>
+                </button>
                 <button>
                     <PlusCircleIcon className="h-5 w-5"/>
                     <p>More</p>
                 </button>
+                <hr className="border-t-[0.1px] border-gray-900" />
             </div>
         </div>
     );
